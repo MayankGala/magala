34ebfe574a7c9a992f713372915b5670e1e7cfd6
Brock <Brock@localhost.localdomain>
2018-10-31 17:53:58 +0530

    Changed return of my_strlin to size_t

---
+++ string.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+size_t my_strlen(char *s)
+{
+  char *p = s;
+  while(*p)
+    ++p;
+  return(p - s);
+}
+int main(void)
+{
+  int i;
+  char *s[] = { "Github Tutorials,Magala" };
+  
+  for(i=0; i<2; ++i)
+    printf("string length of %s = %d\n",s[i],my_strlen(s[i]));
+  return 0;
+}
