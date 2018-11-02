3fbcb1197876f5ed5c2f03ff08c77234b97178d4
magala <mayankgala33.mg@gmail.com>
2018-11-02 15:23:05 +0530

    Added my_strcpy fn

--- string.c
+++ string.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 size_t my_strlen(char *s)
 {
   char *p = s;
@@ -6,12 +7,22 @@ size_t my_strlen(char *s)
        ++p;
   return(p - s);
 }
+char *my_strcpy(char *t, char *s)
+{
+  char *p = t;
+  while(*t++ = *s++);
+     return p;
+}
+
 int main(void)
 {
   int i;
+  char p1[32];
   printf("welcome");  
   char *s[] = { "Github Tutorials,Magala" };
+  
   for(i=0; i<2; ++i)
-    printf("string length of %s = %d\n",s[i],my_strlen(s[i]));
+    printf("string length of %s = %1u\n",s[i],my_strlen(s[i]));
+    printf("%s\n", my_strcpy(p1, "Hello World !!"));
   return 0;
 }
